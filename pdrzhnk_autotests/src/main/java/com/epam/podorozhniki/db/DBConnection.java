769b828caaa8bca124f8f74f91900b48cf4ebe9c
Yan Mikhelkis <yan_mikhelkis@epam.com>
2014-07-24 15:37:25 +0300

    Some hard work was done

--- pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/db/DBConnection.java
+++ pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/db/DBConnection.java
@@ -1,7 +1,4 @@
 package com.epam.podorozhniki.db;
 
 public class DBConnection {
-
-    //wfqf
-    //One more meaningful update
 }
--- pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/pages/Temp.java
+++
@@ -1,4 +0,0 @@
-package com.epam.podorozhniki.pages;
-
-public class Temp {
-}
