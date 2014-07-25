d175cc36f3f721a27e0cc450891bdd9b133dbed8
Viktoriia_Ishchuk <Viktoriia_Ishchuk@epam.com>
2014-07-25 16:34:41 +0300

    initial commit

---
+++ pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/ui/MyRoutesPage.java
@@ -0,0 +1,15 @@
+package com.epam.podorozhniki.ui;
+
+import com.epam.podorozhniki.core.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by Viktoriia_Ishchuk on 7/25/2014.
+ */
+public class MyRoutesPage {
+    public MyRoutesPage(){
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+
+}
