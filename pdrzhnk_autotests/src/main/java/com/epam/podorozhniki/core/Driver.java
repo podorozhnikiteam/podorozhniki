bb27dbafdac3aa70aa04e341abc16db1f4d3acbf
Yan Mikhelkis <yan_mikhelkis@epam.com>
2014-07-28 14:28:28 +0300

    Added init() method to Driver class.

--- pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/core/Driver.java
+++ pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/core/Driver.java
@@ -2,7 +2,13 @@ package com.epam.podorozhniki.core;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
@@ -19,8 +25,28 @@ public class Driver {
     }
 
     public static void init() {
-        driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        Properties properties = new Properties();
+        FileInputStream propFile;
+        try {
+            propFile = new FileInputStream("test.properties");
+            properties.load(propFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+        @SuppressWarnings("unchecked")
+        Enumeration<String> e = (Enumeration<String>) properties.propertyNames();
+        while (e.hasMoreElements()) {
+            String key = e.nextElement();
+            System.setProperty(key, properties.getProperty(key));
+            Reporter.log(key + " - " + properties.getProperty(key), 2, true);
+        }
+        WebDriver driverInput = new FirefoxDriver();
+        driverInput.manage().timeouts().implicitlyWait(
+                Integer.parseInt(System.getProperty("test.timeout")),
+                TimeUnit.SECONDS
+        );
+        Driver.set(driverInput);
     }
 
     public static void tearDown() {
