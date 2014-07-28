4042b9a0b1d4cd3249987d2eb7b272cce535e7fb
Vitalii Kalchuk <Vitalii_Kalchuk@epam.com>
2014-07-28 16:06:45 +0300

    AddTripPage added

--- pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/ui/AddTripPage.java
+++ pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/ui/AddTripPage.java
@@ -21,9 +21,6 @@ public class AddTripPage extends MethodsPage {
 
     private boolean acceptNextAlert = true;
 
-    @FindBy(xpath = "test")
-    public static WebElement xxx;
-
     @FindBy(xpath = "//*[@id='starttimepicker']")
     public static WebElement timeField;
 
