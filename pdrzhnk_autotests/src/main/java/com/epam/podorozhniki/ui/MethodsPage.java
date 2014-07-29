c2d5099d761cc6b82d472bfd5c1636bf7c088452
Viktoriia_Ishchuk <Viktoriia_Ishchuk@epam.com>
2014-07-29 17:12:35 +0300

    Add checkAlert method.

--- pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/ui/MethodsPage.java
+++ pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/ui/MethodsPage.java
@@ -7,13 +7,12 @@ import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertTrue;
 
 public class MethodsPage {
 
@@ -29,6 +28,7 @@ public class MethodsPage {
 		return this;
 	}
 
+    //Get screenshots of current page
     public void getCurrentScreenshots(String filePath, String fileName){
         try {
             String screenPath = filePath;
@@ -39,6 +39,19 @@ public class MethodsPage {
             File screenshot = ((TakesScreenshot) wdriver).getScreenshotAs(OutputType.FILE);
             FileUtils.copyFile(screenshot, new File(screenPath));
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Accept alert with correct alert message.
+    public void checkAlert(String alertMessage) {
+        try {
+            WebDriverWait wait = new WebDriverWait(wdriver, 20);
+            wait.until(ExpectedConditions.alertIsPresent());
+            Alert alert = wdriver.switchTo().alert();
+            assertTrue(alert.getText().matches(".*"+alertMessage+".*"));
+            alert.accept();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
