481ba16be8d3b1a6f19acadf6dbd728dd8c839ac
T"pol <podorozhnikiteam@gmail.com>
2014-07-30 16:39:59 +0300

    commit

---
+++ pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/us/US_1_1_2_8/TripRemovingDriverClass.java
@@ -0,0 +1,40 @@
+package com.epam.podorozhniki.us.US_1_1_2_8;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.epam.podorozhniki.db.*;
+import com.epam.podorozhniki.core.*;
+import com.epam.podorozhniki.ui.*;
+
+
+public class TripRemovingDriverClass {
+	private WebDriver driver = Driver.getInstance(); 
+	private MainPageAfterLogin mainPageAfterLogin;  		
+	
+	
+	@FindBy(xpath = ".//div[@id='routeResults']//tr[2]/td[1]")
+	private WebElement from_point;
+
+	@FindBy(xpath = ".//div[@id='routeResults']//tr[2]/td[2]")
+	private WebElement to_point;
+	
+	private By details_Button = By.xpath("//a[contains(text(),'Details')]");
+	private By nextPage = By.xpath("//div[@id='routeResults']//li[3]/a");
+	
+	public TripRemovingDriverClass() {
+        PageFactory.initElements(Driver.getInstance(), this);
+	}
+	
+	public TripRemovingDriverClass removingAsDriver() { 
+		mainPageAfterLogin = PageFactory.initElements(driver, MainPageAfterLogin.class);
+		
+		
+		return this; 
+	}
+	
+	
+	
+}
