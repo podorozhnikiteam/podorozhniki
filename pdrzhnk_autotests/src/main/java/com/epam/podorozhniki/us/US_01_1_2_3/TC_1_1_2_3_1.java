c6c0349187bbf44c1cdd62023f3c47903f9ca623
Yan Mikhelkis <yan_mikhelkis@epam.com>
2014-07-30 15:56:00 +0300

    changed name to mytripspage

--- pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/us/US_01_1_2_3/TC_1_1_2_3_1.java
+++ pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/us/US_01_1_2_3/TC_1_1_2_3_1.java
@@ -3,12 +3,12 @@ package com.epam.podorozhniki.us.US_01_1_2_3;
 import com.epam.podorozhniki.core.BaseTest;
 import com.epam.podorozhniki.ui.AddTripPage;
 import com.epam.podorozhniki.ui.MainPageAfterLogin;
-import com.epam.podorozhniki.ui.MyRoutesPage;
+import com.epam.podorozhniki.ui.MyTripsPage;
 
 public class TC_1_1_2_3_1 extends BaseTest{
 
     private MainPageAfterLogin mainPageAfterLogin;
-    private MyRoutesPage myRoutesPage;
+    private MyTripsPage myRoutesPage;
     private AddTripPage addTripPage;
-asdf
+
 }
