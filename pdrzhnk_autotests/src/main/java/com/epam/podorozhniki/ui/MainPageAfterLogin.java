e874017153a3a1fa9614a1e5fa594eeeeed5ba9c
Viktoriia_Ishchuk <Viktoriia_Ishchuk@epam.com>
2014-08-01 16:52:03 +0300

    Add US_04_1_1 jbehave autotest

--- pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/ui/MainPageAfterLogin.java
+++ pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/ui/MainPageAfterLogin.java
@@ -1,18 +1,12 @@
 package com.epam.podorozhniki.ui;
 
-import java.util.List;
-
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.NotFoundException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import com.epam.podorozhniki.core.Driver;
 
 /**
@@ -92,4 +86,15 @@ public class MainPageAfterLogin extends MethodsPage {
 		return numFromPage; 
 	}
 
+    public void verifyLogoutButton() {
+        assertTrue(isElementPresent(logoutButton));
+    }
+
+    public void verifyNameDisplayed(String loginName) {
+        assertEquals(username.getText(), loginName);
+    }
+
+    public void verifyLoginError(String incorrectLoginErrorMessage) {
+        assertEquals(error.getText(), incorrectLoginErrorMessage);
+    }
 }
