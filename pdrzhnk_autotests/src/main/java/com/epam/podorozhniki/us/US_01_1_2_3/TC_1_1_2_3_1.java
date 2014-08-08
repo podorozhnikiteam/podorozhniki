6729e01e3cc51692ae1e8c4e276eef35777f6695
Yan Mikhelkis <yan_mikhelkis@epam.com>
2014-08-08 15:57:54 +0300

    Added logging to TC for US1.1.2.3

--- pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/us/US_01_1_2_3/TC_1_1_2_3_1.java
+++ pdrzhnk_autotests/src/main/java/com/epam/podorozhniki/us/US_01_1_2_3/TC_1_1_2_3_1.java
@@ -6,6 +6,7 @@ import com.epam.podorozhniki.ui.AddTripPage;
 import com.epam.podorozhniki.ui.MainPageAfterLogin;
 import com.epam.podorozhniki.ui.MainPageBeforeLogin;
 import com.epam.podorozhniki.ui.MyTripsPage;
+import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -16,14 +17,17 @@ public class TC_1_1_2_3_1 extends BaseTest{
     private MainPageAfterLogin mainPageAfterLogin;
     private MyTripsPage myTripsPage;
     private AddTripPage addTripPage;
+    private Logger log = Logger.getLogger(TC_1_1_2_3_1.class);
 
     @BeforeMethod
     public void login() {
         Driver.getInstance().manage().window().maximize();
         Driver.getInstance().get(System.getProperty("baseUrl"));
+        log.info("Opening home url");
         mainPageBeforeLogin = new MainPageBeforeLogin();
         mainPageBeforeLogin.enterLoginAndPass(System.getProperty("US16.login"), System.getProperty("US16.password"));
         mainPageAfterLogin = mainPageBeforeLogin.pressTheLoginButton();
+        log.info("Logging in");
     }
 
     @Test
@@ -31,13 +35,16 @@ public class TC_1_1_2_3_1 extends BaseTest{
         String expected = "4";
         mainPageAfterLogin = new MainPageAfterLogin();
         myTripsPage = mainPageAfterLogin.goToMyTripsPage();
+        log.info("Opening MyTrips page");
         addTripPage = myTripsPage.gotoAddTripPage();
+        log.info("Opening AddTrip page");
 
         addTripPage.setFromField("Жилянская, 75");
         addTripPage.setToField("Кудряшова, 18");
         addTripPage.clickOnBuildOnMapButton();
         addTripPage.setPassengerSeats(4);
         addTripPage.clickOnCreateTripBtn();
+        log.info("Creating trip");
 
         Assert.assertEquals("Trip hasn't passed validation!", addTripPage.getAlertMessage());
     }
@@ -45,14 +52,17 @@ public class TC_1_1_2_3_1 extends BaseTest{
     @Test
     public void addTrip_withZeroSeats_getError() {
         mainPageAfterLogin = new MainPageAfterLogin();
+        log.info("Opening MyTrips page");
         myTripsPage = mainPageAfterLogin.goToMyTripsPage();
         addTripPage = myTripsPage.gotoAddTripPage();
+        log.info("Opening AddTrip page");
 
         addTripPage.setFromField("Жилянская, 75");
         addTripPage.setToField("Кудряшова, 18");
         addTripPage.clickOnBuildOnMapButton();
         addTripPage.setPassengerSeats(0);
         addTripPage.clickOnCreateTripBtn();
+        log.info("Creating trip");
 
         Assert.assertEquals("Trip hasn't passed validation!", addTripPage.getAlertMessage());
     }
@@ -60,14 +70,17 @@ public class TC_1_1_2_3_1 extends BaseTest{
     @Test
     public void addTrip_withFiveSeats_getError() {
         mainPageAfterLogin = new MainPageAfterLogin();
+        log.info("Opening MyTrips page");
         myTripsPage = mainPageAfterLogin.goToMyTripsPage();
         addTripPage = myTripsPage.gotoAddTripPage();
+        log.info("Opening AddTrip page");
 
         addTripPage.setFromField("Жилянская, 75");
         addTripPage.setToField("Кудряшова, 18");
         addTripPage.clickOnBuildOnMapButton();
         addTripPage.setPassengerSeats(0);
         addTripPage.clickOnCreateTripBtn();
+        log.info("Creating trip");
 
         Assert.assertEquals("Trip hasn't passed validation!", addTripPage.getAlertMessage());
     }
@@ -76,8 +89,10 @@ public class TC_1_1_2_3_1 extends BaseTest{
     public void addTrip_withThreeSeats_threeFreeSeats() {
         String expected = "3";
         mainPageAfterLogin = new MainPageAfterLogin();
+        log.info("Opening MyTrips page");
         myTripsPage = mainPageAfterLogin.goToMyTripsPage();
         addTripPage = myTripsPage.gotoAddTripPage();
+        log.info("Opening AddTrip page");
 
         addTripPage.setFromField("Жилянская, 75");
         addTripPage.setToField("Кудряшова, 18");
@@ -86,6 +101,7 @@ public class TC_1_1_2_3_1 extends BaseTest{
         addTripPage.clickOnCreateTripBtn();
         addTripPage.acceptAlertMessage();
         myTripsPage = addTripPage.clickOnMyTripsPage();
+        log.info("Creating trip");
 
         Assert.assertEquals(expected, myTripsPage.getTotalSeatsNumber(3));
     }
